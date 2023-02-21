df34ee977a97b05b69937305e7cd64c2ee16ccfb
Антон Гаврюхин <ah21@mail.ru>
2023-02-21 11:58:48 +0300

    first commit

---
+++ gh.test.js
@@ -0,0 +1,62 @@
+let page;
+
+beforeEach(async () => {
+  page = await browser.newPage();
+  
+});
+
+afterEach(() => {
+  page.close();
+});
+
+
+describe("Github page tests", () => {
+    beforeEach(async () => {
+    await page.goto("https://github.com/team");
+  });
+  test("The h1 header content'", async () => {
+    const firstLink = await page.$("header div div a");
+    await firstLink.click();
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual("GitHub for teams · Build like the best teams on the planet · GitHub");
+  }, 60000);
+
+  test("The first link attribute", async () => {
+    const actual = await page.$eval("a", link => link.getAttribute('href') );
+    expect(actual).toEqual("#start-of-content");
+  }, 60000);
+
+  test("The page contains Sign in button", async () => {
+    const btnSelector = ".btn-large-mktg.btn-mktg";
+    await page.waitForSelector(btnSelector, {
+      visible: true,
+    });
+    const actual = await page.$eval(btnSelector, link => link.textContent);
+    expect(actual).toContain("Get started with Team");
+  },60000);
+});
+describe("Github page tests - second task", () => {
+  test("The h1 header content'", async () => {
+    await page.goto(
+      "https://github.com/join?plan=business&ref_cta=Get%2520started%2520with%2520Team&ref_loc=team-page-hero&ref_page=%2Fteam"
+    );
+    const firstLink = await page.$("main div div div");
+    await firstLink.click();
+    await page.waitForSelector("h1");
+    const title2 = await page.title();
+    expect(title2).toEqual("Join GitHub · GitHub");
+  }, 11000);
+
+  test("The h1 header content", async () => {
+    await page.goto("https://github.com/about");
+    const title = await page.title();
+    expect(title).toEqual("About · GitHub");
+  }, 11000);
+
+  test("The h1 header content", async () => {
+    await page.goto("https://github.com/");
+    const title = await page.title();
+    expect(title).toEqual("GitHub: Let’s build from here · GitHub");
+  }, 11000);
+});
